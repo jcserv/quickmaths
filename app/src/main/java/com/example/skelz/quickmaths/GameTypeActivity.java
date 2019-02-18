3f5a7e58e49f30198648f2045d48fa9f12288061
Jarrod Servilla <jarrod.servilla@mail.utoronto.ca>
2019-02-18 17:12:51 -0500

    Comments

--- app/src/main/java/com/example/skelz/quickmaths/GameTypeActivity.java
+++ app/src/main/java/com/example/skelz/quickmaths/GameTypeActivity.java
@@ -9,5 +9,6 @@ public class GameTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_type);
+        //yeet
     }
 }
