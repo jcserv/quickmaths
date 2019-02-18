1774b245febe15185f8f8cffc5d02d6c3e15c562
Jarrod Servilla <jarrod.servilla@mail.utoronto.ca>
2019-02-18 17:45:12 -0500

    Buttons and Difficulty screen

--- app/src/main/java/com/example/skelz/quickmaths/GameTypeActivity.java
+++ app/src/main/java/com/example/skelz/quickmaths/GameTypeActivity.java
@@ -2,6 +2,8 @@ package com.example.skelz.quickmaths;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class GameTypeActivity extends AppCompatActivity {
 
@@ -9,6 +11,25 @@ public class GameTypeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_type);
-        //yeet
+    }
+
+    public void addButtonClick(View v) {
+        //Start intent for the mode
+        //Pass addition mode parameter??
+    }
+
+    public void subButtonClick(View v) {
+        //Start intent for the mode
+        //Pass addition mode parameter??
+    }
+
+    public void multButtonClick(View v) {
+        //Start intent for the mode
+        //Pass addition mode parameter??
+    }
+
+    public void divButtonClick(View v) {
+        //Start intent for the mode
+        //Pass addition mode parameter??
     }
 }
