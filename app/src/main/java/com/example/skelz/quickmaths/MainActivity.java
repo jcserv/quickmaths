c03d119a80ecc6bce8c3686e495f67011ca17af7
Jarrod Servilla <jarrod.servilla@mail.utoronto.ca>
2019-02-18 15:53:11 -0500

    Created getRandomInRange

--- app/src/main/java/com/example/skelz/quickmaths/MainActivity.java
+++ app/src/main/java/com/example/skelz/quickmaths/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.skelz.quickmaths;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int min;
+    private int max;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Generate question
+
     }
+
+    public static int getRandomInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("Max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+
 }
