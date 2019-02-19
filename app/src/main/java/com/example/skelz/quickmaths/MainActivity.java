38ae34f24e957682c20f8a77a0fb4f590b722e76
Anthony Granic <granicanthony@gmail.com>
2019-02-19 13:04:01 -0500

    UI updates

--- app/src/main/java/com/example/skelz/quickmaths/MainActivity.java
+++ app/src/main/java/com/example/skelz/quickmaths/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     //private int lower;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent i = new Intent(getApplicationContext(), GameTypeActivity.class);
+        try{TimeUnit.SECONDS.sleep(1);}
+        catch(Exception e){ }
+
         startActivity(i);
         //int x = getRandomInRange(lower, upper);
         //int y = getRandomInRange(lower, upper);
