512e9e52c302f54221dc44d91bb15c23095e5b2c
Jarrod Servilla <jarrod.servilla@mail.utoronto.ca>
2019-02-19 10:33:36 -0500

    Created new activities

---
+++ app/src/main/java/com/example/skelz/quickmaths/ResultsScreen.java
@@ -0,0 +1,13 @@
+package com.example.skelz.quickmaths;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results_screen);
+    }
+}
